13d9706efe83eab25fadcbea4861b962009cd473
LyrVe <89419604+LyrVe@users.noreply.github.com>
2021-10-22 07:31:18 +0700

    Add files via upload

---
+++ Tuần 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bài_7
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập kích cỡ ma trận vuông, n = ");
+            int n = int.Parse(Console.ReadLine());
+            double[,] matrix = new double [n, n];
+            double sum = 0; 
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Nhập phần tử Matrix[{0}, {1}] = ", i, j);
+                    matrix[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        sum += matrix[i, j];
+                    }    
+                }
+            }
+            Console.Write("Tổng giá trị phần tử trên đường chéo chính = {0}", sum);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
